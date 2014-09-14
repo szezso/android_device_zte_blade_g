befb42490d1dc3fe04a4d1736fd587217a2fbf09
szezso <szezsoo@gmail.com>
2014-09-14 15:30:56 +0200

    Blade_G: Add-in msm7x27a-common

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -14,11 +14,141 @@
 # limitations under the License.
 #
 
+#
+# Copyright 2014 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# WARNING: This line must come *before* including the proprietary
+# variant, so that it gets overwritten by the parent (which goes
+# against the traditional rules of inheritance).
+# The proprietary variant sets USE_CAMERA_STUB := false, this way
+# we use the camera stub when the vendor tree isn't present, and
+# the true camera library when the vendor tree is available.
+USE_CAMERA_STUB := true
+
+TARGET_NO_BOOTLOADER := true
+TARGET_NO_RADIOIMAGE := true
+
+TARGET_SPECIFIC_HEADER_PATH := device/zte/blade_g/include
+
+# Architecture and CPU
+TARGET_ARCH := arm
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_CPU_VARIANT := cortex-a5
+
+TARGET_BOARD_PLATFORM := msm7x27a
+TARGET_BOARD_PLATFORM_GPU := qcom-adreno200
+
+ARCH_ARM_HAVE_TLS_REGISTER := true
+
+TARGET_ARCH_LOWMEM := true
+
+# Qualcomm hardware
+BOARD_USES_QCOM_HARDWARE := true
+COMMON_GLOBAL_CFLAGS += -DQCOM_HARDWARE -DQCOM_BSP
+TARGET_USES_ION := true
+TARGET_USES_QCOM_BSP := true
+
+BOARD_WANTS_EMMC_BOOT := true
+
+# Compiler flags
+TARGET_GLOBAL_CFLAGS += -mfloat-abi=softfp -mfpu=neon -mtune=cortex-a5
+TARGET_GLOBAL_CPPFLAGS += -mfloat-abi=softfp -mfpu=neon -mtune=cortex-a5
+
+# Audio
+BOARD_USES_LEGACY_ALSA_AUDIO := true
+TARGET_QCOM_AUDIO_VARIANT := legacy
+
+# Bluetooth
+BOARD_HAVE_BLUETOOTH := true
+
+# Camera
+COMMON_GLOBAL_CFLAGS += -DMR0_CAMERA_BLOB -DNEEDS_VECTORIMPL_SYMBOLS
+
+# GPS
+BOARD_USES_QCOM_GPS := true
+BOARD_VENDOR_QCOM_GPS_LOC_API_AMSS_VERSION := 50000
+BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := $(TARGET_BOARD_PLATFORM)
+
+# Graphics
+BOARD_ADRENO_DECIDE_TEXTURE_TARGET := true
+BOARD_EGL_CFG := device/zte/blade_g/configs/egl.cfg
+BOARD_EGL_WORKAROUND_BUG_10194508 := true
+BOARD_USE_MHEAP_SCREENSHOT := true
+TARGET_DOESNT_USE_FENCE_SYNC := true
+TARGET_QCOM_DISPLAY_VARIANT := legacy
+USE_OPENGL_RENDERER := true
+
+# Hardware
+BOARD_HARDWARE_CLASS := device/zte/blade_/cmhw
+
+# Kernel
+BOARD_KERNEL_BASE := 0x00200000
+BOARD_KERNEL_CMDLINE := androidboot.hardware=huawei androidboot.selinux=permissive
+BOARD_PAGE_SIZE := 2048
+TARGET_KERNEL_SOURCE := kernel/zte/blade_g
+
+# Lights
+TARGET_PROVIDES_LIBLIGHTS := true
+
+# Media
+TARGET_QCOM_LEGACY_MMPARSER := true
+TARGET_QCOM_MEDIA_VARIANT := legacy
+
+# Partitions
+BOARD_BOOTIMAGE_PARTITION_SIZE := 0x800000
+BOARD_CACHEIMAGE_PARTITION_SIZE := 201326592 # 0xC000000
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x1400000
+
+BOARD_FLASH_BLOCK_SIZE := 131072
+TARGET_USERIMAGES_USE_EXT4 := true
+
+# Recovery
+BOARD_HAS_NO_SELECT_BUTTON := true
+BOARD_UMS_LUNFILE := /sys/class/android_usb/android0/f_mass_storage/lun%d/file
+TARGET_RECOVERY_INITRC := device/zte/blade_g/recovery/init.rc
+TARGET_RECOVERY_PIXEL_FORMAT := RGBX_8888
+
+# SELinux
+BOARD_SEPOLICY_DIRS += device/zte/blade_g/sepolicy
+
+BOARD_SEPOLICY_UNION += \
+    file_contexts \
+    file.te
+
+# USB
+BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
+BOARD_VOLD_MAX_PARTITIONS := 19
+TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/class/android_usb/android0/f_mass_storage/lun%d/file
+TARGET_USE_CUSTOM_SECOND_LUN_NUM := 1
+
+# Web Rendering
+ENABLE_WEBGL := true
+PRODUCT_PREBUILT_WEBVIEWCHROMIUM := yes
+TARGET_FORCE_CPU_UPLOAD := true
+
+# Wi-Fi
+BOARD_WPA_SUPPLICANT_DRIVER := NL80211
+BOARD_HOSTAPD_DRIVER := NL80211
+TARGET_CUSTOM_WIFI := ../../device/zte/blade_g/libhardware_legacy/wifi/wifi.c
+WPA_SUPPLICANT_VERSION := VER_0_8_X
+
 # Inherit from the proprietary version
 -include vendor/huawei/u8833/BoardConfigVendor.mk
-
-# Inherit from the common msm7x27a definitions
--include device/zte/msm7x27a-common/BoardConfigCommon.mk
 
 # Architecture and CPU
 TARGET_CPU_SMP := true
--- blade_g.mk
+++ blade_g.mk
@@ -14,8 +14,121 @@
 # limitations under the License.
 #
 
-# Inherit the msm7x27a-common definitions
-$(call inherit-product, device/zte/msm7x27a-common/msm7x27a.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# The GPS configuration appropriate for this device.
+$(call inherit-product, device/common/gps/gps_eu_supl.mk)
+
+DEVICE_PACKAGE_OVERLAYS += device/huawei/blade_g/overlay
+
+PRODUCT_AAPT_CONFIG := normal hdpi
+PRODUCT_AAPT_PREF_CONFIG := hdpi
+
+PRODUCT_TAGS += dalvik.gc.type-precise
+
+# Packages
+PRODUCT_PACKAGES += \
+    audio.a2dp.default \
+    audio.primary.msm7x27a \
+    audio.usb.default \
+    audio_policy.msm7x27a \
+    libaudioutils
+
+PRODUCT_PACKAGES += \
+    copybit.msm7x27a \
+    gralloc.msm7x27a \
+    hwcomposer.msm7x27a \
+    libtilerenderer
+
+PRODUCT_PACKAGES += \
+    librs_jni \
+    LiveWallpapers \
+    LiveWallpapersPicker \
+    VisualizationWallpapers
+
+PRODUCT_PACKAGES += \
+    libmm-omxcore \
+    libOmxCore \
+    libstagefrighthw
+
+PRODUCT_PACKAGES += \
+    gps.msm7x27a \
+    lights.msm7x27a
+
+PRODUCT_PACKAGES += \
+    com.android.future.usb.accessory \
+    hwmac \
+    make_ext4fs \
+    setup_fs
+
+# Files
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,device/zte/blade_g/rootdir,root)
+
+# Permissions
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
+    frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
+    frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml \
+    frameworks/native/data/etc/android.hardware.sensor.compass.xml:system/etc/permissions/android.hardware.sensor.compass.xml \
+    frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
+    frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
+    frameworks/native/data/etc/android.hardware.touchscreen.multitouch.distinct.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.distinct.xml \
+    frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
+    frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
+    frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
+    frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
+    packages/wallpapers/LivePicker/android.software.live_wallpaper.xml:system/etc/permissions/android.software.live_wallpaper.xml
+
+# Properties
+PRODUCT_PROPERTY_OVERRIDES += \
+    headset.hook.delay=500
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.bluetooth.remote.autoconnect=true \
+    ro.bluetooth.request.master=true \
+    ro.bt.bdaddr_path=/data/misc/bluedroid/bdaddr \
+    ro.qualcomm.bluetooth.dun=true \
+    ro.qualcomm.bluetooth.ftp=true
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.cwm.enable_key_repeat=true
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    debug.composition.type=dyn \
+    debug.hwc.dynThreshold=1.9 \
+    ro.bq.gpu_to_cpu_unsupported=1 \
+    ro.max.fling_velocity=4000 \
+    ro.opengles.version=131072 \
+    ro.sf.lcd_density=240
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.heapgrowthlimit=32m \
+    dalvik.vm.heapsize=96m \
+    ro.config.low_ram=true
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    com.qc.hardware=true \
+    dev.pm.dyn_sample_period=700000 \
+    dev.pm.dyn_samplingrate=1 \
+    ro.vendor.extension_library=/system/lib/libqc-opt.so
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ril.subscription.types=NV,RUIM \
+    rild.libargs=-d/dev/smd0 \
+    ro.telephony.call_ring.delay=3000 \
+    ro.telephony.call_ring.multiple=false
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.sys.usb.config=mtp,adb \
+    ro.vold.umsdirtyratio=50
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.webview.provider=classic
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    wifi.interface=wlan0 \
+    wifi.supplicant_scan_interval=60
 
 DEVICE_PACKAGE_OVERLAYS += device/zte/blade_g/overlay
 
@@ -47,5 +160,3 @@ PRODUCT_PROPERTY_OVERRIDES += \
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full.mk)
 
 $(call inherit-product, frameworks/native/build/phone-hdpi-512-dalvik-heap.mk)
-
-$(call inherit-product, vendor/huawei/u8833/u8833-vendor.mk)
